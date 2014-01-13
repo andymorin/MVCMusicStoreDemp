de70353a71ecc705769a0fb84576448d75233c74
andymorin <andy.morin@gmail.com>
2014-01-13 12:41:22 -0600

    AM - Adding Project Files
    AM - Adding Project Files

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public string Index()
+        {
+            return "Hello from Store.Index()";
+        }
+
+        public string Browse(string genre)
+        {
+            //return "Hello from Store.Browse()";
+            string Message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+
+            return Message;
+        }
+
+        public string Details(int id)
+        {
+
+              // return "Hello from Store.Details()";
+            string Message = "Store.Details, ID = " + id;
+            return Message;
+        }
+
+    }
+}
